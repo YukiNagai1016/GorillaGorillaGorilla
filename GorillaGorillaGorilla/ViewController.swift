7ad8a6f46342c12c7d41eda6fa1d581df7f3abaa
優樹永井 <wadachi0505@gmail.com>
2019-08-11 20:58:21 +0900

    Initial Commit

---
+++ GorillaGorillaGorilla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorillaGorillaGorilla
+//
+//  Created by 優樹永井 on 2019/08/11.
+//  Copyright © 2019 com.nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
